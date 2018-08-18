81db55fa9deb45fe0378245e7debf99137bbd82e
Pfuster12 <p.fuster.a@gmail.com>
2018-08-18 20:10:26 +0100

    Added header and footer item layout options.

--- boilercycle/src/main/java/com/android/lightmass/boilercycle/BoilerCycle.kt
+++ boilercycle/src/main/java/com/android/lightmass/boilercycle/BoilerCycle.kt
@@ -7,6 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
+// constants
+private const val BOILERCYCLE_ITEM = 100
+private const val BOILERCYCLE_HEADER = 101
+private const val BOILERCYCLE_FOOTER = 102
+
 /**
  * BoilerCycle is a one-class lightweight Android Library that handles creating the
  * tedious RecyclerView + ViewHolder pattern set up easily in a few function calls.
@@ -19,6 +24,14 @@ class BoilerCycle private constructor() {
      */
     // holds the layout id to inflate in the view holder
     private var mResId = -1
+    // holds the header layout id to inflate in the view holder
+    private var mHeaderResId = -1
+    // holds the footer layout id to inflate in the view holder
+    private var mFooterResId = -1
+    // bool check if user is using a header
+    private var isUsingHeader = false
+    // bool check if user is using a header
+    private var isUsingFooter = false
     // check if user is using simple item
     private var isUsingSimpleItem = false
     // adapter variable
@@ -44,6 +57,28 @@ class BoilerCycle private constructor() {
     }
 
     /**
+     * Grabs the header layout
+     */
+    fun useHeader(resId: Int): BoilerCycle {
+        mHeaderResId = resId
+        // set the bool to true
+        isUsingHeader = true
+        // return this for chaining
+        return this
+    }
+
+    /**
+     * Grabs the header layout
+     */
+    fun useFooter(resId: Int): BoilerCycle {
+        mFooterResId = resId
+        // set the bool to true
+        isUsingFooter = true
+        // return this for chaining
+        return this
+    }
+
+    /**
      * Sets the adapter by creating a view holder using the res Id provided.
      * The on bind method is passed through a lambda in the function constructor.
      * Finally the view holder on click is passed through as a lambda, to go into the
@@ -55,8 +90,20 @@ class BoilerCycle private constructor() {
             // init the adapter,
             adapter = object : RecyclerView.Adapter<ViewHolder>() {
                 // creates viewholder
-                override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-                        ViewHolder(LayoutInflater.from(context).inflate(mResId, parent, false), onClick)
+                override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+                    return when (viewType) {
+                        BOILERCYCLE_HEADER -> {
+                            ViewHolder(LayoutInflater.from(context).inflate(mHeaderResId, parent, false), onClick)
+                        }
+                        BOILERCYCLE_ITEM -> {
+                            ViewHolder(LayoutInflater.from(context).inflate(mResId, parent, false), onClick)
+                        }
+                        BOILERCYCLE_FOOTER -> {
+                            ViewHolder(LayoutInflater.from(context).inflate(mFooterResId, parent, false), onClick)
+                        }
+                        else -> ViewHolder(LayoutInflater.from(context).inflate(mResId, parent, false), onClick)
+                    }
+                }
 
                 // gets the item count from the data list
                 override fun getItemCount() = data.size
@@ -64,6 +111,36 @@ class BoilerCycle private constructor() {
                 // on binds through lambda function
                 override fun onBindViewHolder(holder: ViewHolder, position: Int) {
                     onBind(holder, position)
+                }
+
+                // get whether item is a header, footer or item
+                override fun getItemId(position: Int): Long {
+                    when {
+                        // only header
+                        isUsingHeader && !isUsingFooter -> {
+                            return when (position) {
+                                0 -> BOILERCYCLE_HEADER.toLong()
+                                else -> BOILERCYCLE_ITEM.toLong()
+                            }
+                        }
+                        // only footer
+                        isUsingFooter && !isUsingHeader -> {
+                            return when (position) {
+                                data.size -> BOILERCYCLE_FOOTER.toLong()
+                                else -> BOILERCYCLE_ITEM.toLong()
+                            }
+                        }
+                        // using both
+                        isUsingHeader && isUsingFooter -> {
+                            return when (position) {
+                                0 -> BOILERCYCLE_HEADER.toLong()
+                                data.size + 1 -> BOILERCYCLE_FOOTER.toLong()
+                                else -> BOILERCYCLE_ITEM.toLong()
+                            }
+                        }
+                        // else return item type
+                        else -> return BOILERCYCLE_ITEM.toLong()
+                    }
                 }
             }
             // and sets the adapter,
